678e3a970bc7cd039e5e6c170f28cbef810afe85
storvus <anton.scherbatov@invitae.com>
2024-02-23 19:17:56 +0100

    feat(main): added opportunity to upload images for articles

--- pythonsucks/article/admin.py
+++ pythonsucks/article/admin.py
@@ -1,11 +1,22 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
 from django.contrib import admin
 
 from pythonsucks.article.models import Article
 
 
+class ArticleAdminForm(forms.ModelForm):
+    body = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
 class ArticleAdmin(admin.ModelAdmin):
-    model = Article
+    form = ArticleAdminForm
     prepopulated_fields = {"slug": ["title"]}
+
 
 
 admin.site.register(Article, ArticleAdmin)
--- pythonsucks/urls.py
+++ pythonsucks/urls.py
@@ -1,10 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include('pythonsucks.article.urls', namespace="article")),
     path("admin/", admin.site.urls),
-]
+    path("", include('pythonsucks.article.urls', namespace="article")),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
